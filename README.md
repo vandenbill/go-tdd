af814f3b33385b92666f051b5837eba1d2c8ce01
vandenbill <nailanmnabil@gmail.com>
2023-02-06 10:47:03 +0700

    write test

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ TDD is currently a software development method that is widely used considering t
 tdd will be very difficult to do if we don't have clear requirements in our system (even though tdd also helps us imagine what our requirements are), so let's describe what is happening in our queue system
 - an queue has 0 length on construction
 - an queue is empty on construction
-- after n prepend to an empty queue, the queue is non empty and its length equal n
-- after we prepend a value, the value must be in frist order
-- if we take a data fromq queue, the data must be in the last order
+- if we dequeue an empty queue, we got an error: ErrNoSuchElement
+- after n enqueue to an empty queue, the queue is non empty and its length equal n
+- if we dequeue from queue, the data must be in the last order
 
 above are the requirements of a queue, even though the requirements above do not fully describe what is happening in the queue, but it is enough for us to start writing tests, and refactor
--- queue_test.go
+++ queue_test.go
@@ -1,20 +1,53 @@
-package main_test
+package queue_test
 
 import (
 	"testing"
+
+	queue "github.com/vandenbill/go-tdd"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // - an queue has 0 length on construction
 // - an queue is empty on construction
-// - after n prepend to an empty queue, the queue is non empty and its length equal n
-// - after we prepend a value, the value must be in frist order
-// - if we take a data fromq queue, the data must be in the last order
+// - if we dequeue an empty queue, we got an error: ErrNoSuchElement
+// - after n enqueue to an empty queue, the queue is non empty and its length equal n
+// - if we dequeue from queue, the data must be in the last order
 
 func TestQueue(t *testing.T) {
+	q := queue.NewQueue()
+
 	t.Run("an queue has 0 length on construction", func(t *testing.T) {
-		q := queue.New()
-		assert.True(t, q.isEmpty())
+		assert.Equal(t, 0, q.Length)
+	})
+
+	t.Run("an queue is empty on construction", func(t *testing.T) {
+		assert.True(t, q.IsEmpty())
+	})
+
+	t.Run("if we dequeue an empty queue, we got an error: ErrNoSuchElement", func(t *testing.T) {
+		_, err := q.Dequeue()
+		assert.Equal(t, queue.ErrNoSuchElement, err)
+	})
+
+	n1 := queue.NewNode(1)
+	n2 := queue.NewNode(2)
+	n3 := queue.NewNode(3)
+	n4 := queue.NewNode(4)
+	n5 := queue.NewNode(5)
+	q.Enqueue(n1)
+	q.Enqueue(n2)
+	q.Enqueue(n3)
+	q.Enqueue(n4)
+	q.Enqueue(n5)
+
+	t.Run("after n enqueue to an empty queue, the queue is non empty and its length equal n", func(t *testing.T) {
+		assert.Equal(t, 5, q.Length)
+	})
+
+	t.Run("if we dequeue from queue, the data must be in the last order", func(t *testing.T) {
+		v, err := q.Dequeue()
+		assert.Nil(t, err)
+		assert.Equal(t, n1.Value, v)
 	})
 }
